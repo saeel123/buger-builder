e51764a496a55fb6f3df5682a971049f1b24c720
Saeel <saeelparulekar>
2020-05-30 00:18:09 +0530

    added component will update and folder sturcture update

--- src/components/Burger/OrderSummarry/OrderSumarry.js
+++ src/components/Burger/OrderSummarry/OrderSumarry.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
-        return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey} :</span> {props.ingredients[igKey]}</li>
-    });
+class OrderSummary extends Component  {
+    render () {
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>Ingredients</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Price: { props.price.toFixed(2) }</strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-            <Button btnType="Success" clicked={props.purchaseContinue}>Buy</Button>
-        </Aux>
-    )
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+            return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey} :</span> {this.props.ingredients[igKey]}</li>
+        });
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>Ingredients</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Price: { this.props.price.toFixed(2) }</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinue}>Buy</Button>
+            </Aux>
+        )
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
