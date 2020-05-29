869ef63bdf9ba540cbd900ff2551fcfcdd69661d
Saeel <saeelparulekar>
2020-05-29 09:56:58 +0530

    added sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import styles from './Layout.module.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 
 const layout = (props) => (
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <main className={styles.Content}>
             {props.children}
         </main>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,8 +3,8 @@ import React from 'react';
 import classes from './Logo.module.css';
 import burgerLogo from '../../assets/images/original.png'
 
-const logo = () => (
-    <div className={classes.Logo}>
+const logo = (props) => (
+    <div className={classes.Logo} style={{height: props.height}}>
         <img src={burgerLogo} alt="MyBurger" />
     </div>
 );
