133f9f9e391503a118deea78097e6affaa079aba
Saeel <saeelparulekar>
2020-06-08 23:46:28 +0530

    added custom input element

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,14 +4,53 @@ import axios from '../../../axios-orders'
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'email',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Email'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Street'
+                },
+                value: ''
+            },
+            postalCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Pincode'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'},
+                    ]
+                },
+                value: ''
+            },
         },
         loading: false
     }
@@ -49,12 +88,11 @@ class ContactData extends Component {
     render() {
             let form =(
                         <form>
-                            <input className={classes.Input} type="text" name="name" placeholder="Enter Your Name"/>
-                            <input className={classes.Input} type="text" name="email" placeholder="Enter Your Email"/>
-                            <input className={classes.Input} type="text" name="street" placeholder="Enter Your Street"/>
-                            <input className={classes.Input} type="text" name="postalCode" placeholder="Enter Your Postal Code "/>
-                            <Button clicked btnType="Danger">CANCEL</Button>
-                            <Button btnType="Success" clicked={this.orderHandler}>SUBMIT</Button>
+                            <Input elementType="input" elementConfig="..." value="" />
+                            <Input inputtype="input" type="text" name="email" placeholder="Enter Your Email"/>
+                            <Input inputtype="input" type="text" name="street" placeholder="Enter Your Street"/>
+                            <Input inputtype="input" type="text" name="postalCode" placeholder="Enter Your Postal Code "/>
+                            <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
                         </form>
             );  
                             
