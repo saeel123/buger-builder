abb9a97e7d68b2f89c16812a813f7e5e2d06d4f0
Saeel Parulekar <saeelparulekar>
2020-07-09 18:14:08 +0530

    added fetch user specific orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -61,10 +61,11 @@ export const fetchOrdersStart = () => {
     }
 }
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart());
-        axios.get('/orders.json?auth='+token).then(response => {
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+        axios.get('/orders.json'+queryParams).then(response => {
             const fetchOrders = [];
             for(let key in response.data){
                 fetchOrders.push({
